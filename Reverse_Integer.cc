590eb784a5749d8545c0091a87664e294d455d55
westu <zhqy46@gmail.com>
2015-12-07 23:25:24 +0800

    Solution for #7 Reverse Integer
    https://leetcode.com/problems/reverse-integer/
    
    Notice the range of type int.

---
+++ Reverse_Integer.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int symbol = x < 0 ? -1 : 1;
+        x = abs(x);
+        long long ans = 0;
+        while (x > 0) {
+            ans = ans * 10 + x %10;
+            x /= 10;
+        }
+        if (abs(ans) >= ((long long)1 << 32 - 1)) {
+            return 0;
+        }
+        return ans * symbol;
+    }
+};
