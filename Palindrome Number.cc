9e303c09bc831d068e039fc2cc60be4b3c80eb0b
westu <zhqy46@gmail.com>
2015-12-08 21:45:14 +0800

    Solution for #8 Palindrome Number
    https://leetcode.com/problems/palindrome-number/

---
+++ Palindrome Number.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x == 0){
+            return true;
+        }
+        string str;
+        while (x > 0) {
+            str += '0' + (x % 10);
+            x /= 10;
+        }
+        int len = static_cast<int>(str.length());
+        for (int i = 0; i <= len / 2 - 1; i++) {
+            if (str[i] != str[len - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
