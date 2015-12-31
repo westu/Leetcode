2addf0063d7f3e37d1d1dd4c4a8aa93a35cc3da8
westu <zhqy46@gmail.com>
2015-12-31 23:20:49 +0800

    # 34
    https://leetcode.com/problems/search-for-a-range/

---
+++ Search for a Range.cc
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int s = 0;
+        int t = static_cast<int>(nums.size());
+        while (s < t) {
+            int mid = s + (t - s) / 2;
+            if (nums[mid] >= target) {
+                t = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        vector<int> ans(2);
+        ans[0] = nums[s] == target ? s : -1;
+        s = 0;
+        t = static_cast<int>(nums.size());
+        while (s < t) {
+            int mid = s + (t - s) / 2;
+            if (nums[mid] <= target) {
+                s = mid + 1;
+            } else {
+                t = mid;
+            }
+        }
+        ans[1] = nums[t - 1] == target ? t - 1 : -1;
+        return ans;
+    }
+};
