6d663d0faf195a0c3e9280d9b0e5d40ac0dbe5ea
westu <zhqy46@gmail.com>
2016-01-28 22:21:11 +0800

    Create 52. N-Queens II.cc
    https://leetcode.com/problems/n-queens-ii/

---
+++ 52. N-Queens II.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int ans = 0;
+        vector<bool> col(n, true), lch(2 * n, true), rch(2 * n, true);
+        dfs(0, col, lch, rch, ans, n);
+        return ans;
+    }
+
+private:
+    void dfs(int x, vector<bool>& col, vector<bool>& lch,
+             vector<bool>& rch, int& ans, int n) {
+        if (x == n) {
+            ++ans;
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (col[i] == true && lch[n - 1 - (x - i)] == true &&
+                rch[i + x] == true) {
+                col[i] = false;
+                lch[n - 1 - (x - i)] = false;
+                rch[i + x] = false;
+                dfs(x + 1, col, lch, rch, ans, n);
+                col[i] = true;
+                lch[n - 1 - (x - i)] = true;
+                rch[i + x] = true;
+            }
+        }
+    }
+};
