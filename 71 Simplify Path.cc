e430347965f2732812df0c82bd16c9f2cd4bf662
Qingyu Zhang <zhqy46@gmail.com>
2016-03-07 22:53:26 +0800

    Create 71 Simplify Path.cc

---
+++ 71 Simplify Path.cc
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /**
+     * @param path the original path
+     * @return the simplified path
+     */
+    string simplifyPath(string& path) {
+        vector<string> dirs;
+        for (size_t i = 0; i < path.size();) {
+            ++i;
+            size_t j = path.find('/', i);
+            string sub_dir = path.substr(i, j - i);
+            if (sub_dir.empty() == false && sub_dir != ".") {
+                if (sub_dir == "..") {
+                    if (dirs.empty() == false) {
+                        dirs.pop_back();
+                    }
+                } else {
+                    dirs.push_back(sub_dir);
+                }
+            }
+            i = j;
+        }
+        string ans;
+        if (dirs.empty() == true) {
+            ans = "/";
+        } else {
+            for (auto it = dirs.begin(); it != dirs.end(); ++it) {
+                ans += "/" + (*it);
+            }
+        }
+        return ans;
+    }
+};
