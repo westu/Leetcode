1196ef6e0f6541aebfe08bac21b6a20c86916cd8
Qingyu Zhang <zhqy46@gmail.com>
2016-04-13 11:03:58 +0800

    Update and rename Longest_Substring_Without_Repeating_Characters.cpp to 3 Longest Substring Without Repeating Characters.cc

--- 3 Longest Substring Without Repeating Characters.cc
+++ 3 Longest Substring Without Repeating Characters.cc
@@ -4,11 +4,13 @@ public:
         if (s.empty() == true) {
             return 0;
         }
-        unordered_map<char, int> p; // record the last place of current character's appearance, false or not in map means it's the first time of this charactor
+        unordered_map<char, int> p;
+        // record the last place of current character's appearance, false or not in map means it's the first time of this charactor
         int subscript_f = 0;
         int ans = 0;
         
-        vector<int> f; // f[i] records the longest substring whithout repeating characters which ends with s[i - 1]
+        vector<int> f;
+        // f[i] records the longest substring whithout repeating characters which ends with s[i - 1]
         f.push_back(0); // f[0] = 0
         
         for (string::iterator it = s.begin(); it != s.end(); it++) {
