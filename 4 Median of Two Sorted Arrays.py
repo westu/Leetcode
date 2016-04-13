8a19975dc4357899eebc2e43e59b2a911e5d0487
Qingyu Zhang <zhqy46@gmail.com>
2016-04-13 11:14:49 +0800

    Update and rename MedianofTwoSortedArrays.py to 4 Median of Two Sorted Arrays.py

--- 4 Median of Two Sorted Arrays.py
+++ 4 Median of Two Sorted Arrays.py
@@ -19,15 +19,12 @@ class Solution(object):
             return nums2[k - 1]
         if len(nums2) == 0:
             return nums1[k - 1]
-
         if k == 1:
             return min(nums1[0], nums2[0])
-
         if len(nums1) < k / 2:
             return self.findKthInSortedArrays(nums1, nums2[k / 2 : ], k - k / 2)
         if len(nums2) < k / 2:
             return self.findKthInSortedArrays(nums1[k / 2 : ], nums2, k - k / 2)
-
         if nums2[k / 2 - 1] < nums1[k / 2 - 1]:
             return self.findKthInSortedArrays(nums1, nums2[k / 2 : ], k - k / 2)
         else:
