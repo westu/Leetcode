1c60c02cdbd86137c8d9b80c893d252da40c384b
Qingyu Zhang <zhqy46@gmail.com>
2016-07-04 21:26:27 +0800

    Create 371 Sum of Two Integers.cc

---
+++ 371 Sum of Two Integers.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        while (b != 0) {
+            int c = a ^ b;
+            b = (a & b)  << 1;
+            a = c;
+        }
+        return a;
+    }
+};
