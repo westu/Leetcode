501ef55fc283bef764219a0bdd4f69feb64be757
Qingyu Zhang <zhqy46@gmail.com>
2016-07-24 21:19:02 +0800

    Create 258 Add Digits.cc

---
+++ 258 Add Digits.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num % 9 != 0) {
+            return num % 9;
+        } else {
+            return num == 0 ? 0 : 9;
+        }
+    }
+};
