42d3cec06e4fe687e98301ad44556c2775991395
Qingyu Zhang <zhqy46@gmail.com>
2016-08-28 00:25:24 +0800

    Update 313 Super Ugly Number.cc

--- 313 Super Ugly Number.cc
+++ 313 Super Ugly Number.cc
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    /**
+     * @param n a positive integer
+     * @param primes the given prime list
+     * @return the nth super ugly number
+     */
     int nthSuperUglyNumber(int n, vector<int>& primes) {
         int l = static_cast<int>(primes.size());
         vector<int> ptr(l, 0);
@@ -8,15 +13,15 @@ public:
         num_list.push_back(1);
         
         for (int i = 1; i < n; ++i) {
-            priority_queue<int, vector<int>, greater<int> > heap;
-            for (int i = 0; i < l; ++i) {
-                heap.push(num_list.at(ptr.at(i)) * primes.at(i));
+            int min_num = num_list.at(ptr.at(0)) * primes.at(0);
+            for (int i = 1; i < l; ++i) {
+                min_num = min(min_num, num_list.at(ptr.at(i)) * primes.at(i));
             }
-            num_list.push_back(heap.top());
+            num_list.push_back(min_num);
             
             for (int i = 0; i < l; ++i) {
-                while (num_list.at(ptr.at(i)) * primes.at(i) <= num_list.back()) {
-                    ptr.at(i) = ptr.at(i) + 1;
+                if (num_list.at(ptr.at(i)) * primes.at(i) == num_list.back()) {
+                    ++ptr.at(i);
                 }
             }
         }
