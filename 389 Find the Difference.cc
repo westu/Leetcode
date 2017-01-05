3e35a3bf1fc7fdad0029707378d787290e1f08e5
Qingyu Zhang <zhqy46@gmail.com>
2017-01-05 19:29:36 +0800

    Create 389 Find the Difference.cc

---
+++ 389 Find the Difference.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum1(0), sum2(0);
+        for (char c : s) {
+            sum1 += c - 'a';
+        }
+        for (char c : t) {
+            sum2 += c - 'a';
+        }
+        return sum2 - sum1 + 'a';
+    }
+};
