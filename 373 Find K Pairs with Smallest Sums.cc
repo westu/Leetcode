78cc08a014be8bddfd3ece821b0a508f7c64897a
Qingyu Zhang <zhqy46@gmail.com>
2018-01-28 00:09:48 +0800

    Create 373 Find K Pairs with Smallest Sums.cc

---
+++ 373 Find K Pairs with Smallest Sums.cc
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    vector<pair<int, int>> ans;
+    if (k == 0 || nums1.empty() || nums2.empty()) {
+      return ans;
+    }
+    
+    multimap<int, pair<int, int>> min_heap;
+    int len1 = static_cast<int>(nums1.size());
+    int len2 = static_cast<int>(nums2.size());
+    for (int i = 0; i < len1; ++i) {
+      min_heap.insert(make_pair(nums1.at(i) + nums2.at(0), make_pair(i, 0)));
+    }
+    
+    while (ans.size() < k && !min_heap.empty()) {
+      auto min_pair = min_heap.begin();
+      min_heap.erase(min_heap.begin());
+      int pos1 = min_pair->second.first;
+      int pos2 = min_pair->second.second;
+      ans.push_back(make_pair(nums1.at(pos1), nums2.at(pos2)));
+      if (pos2 + 1 < len2) {
+        min_heap.insert(make_pair(nums1.at(pos1) + nums2.at(pos2 + 1), make_pair(pos1, pos2 + 1)));
+      }
+    }
+    
+    return ans;
+  }
+};
